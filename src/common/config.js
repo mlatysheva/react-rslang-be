7c0c3a5477bd0b6cf29250dc4b10b52e999b858b
Maria Latysheva <61849781+mlatysheva@users.noreply.github.com>
2022-02-17 10:38:01 +0300

    Update config.js

--- src/common/config.js
+++ src/common/config.js
@@ -15,7 +15,7 @@ module.exports = {
   MIN_PASSWORD_LENGTH: 8,
   LOGS_DIR: path.join(__dirname, '../../logs'),
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  JWT_EXPIRE_TIME: '4h',
+  JWT_EXPIRE_TIME: '24h',
   JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
-  JWT_REFRESH_EXPIRE_TIME: 4.5 * 60 * 60
+  JWT_REFRESH_EXPIRE_TIME: 24 * 60 * 60
 };
